4b3690a91f53c389a0ce49f453290c3a4be2ee4d
Natalia Petelina <ss@ss.ru>
2022-09-10 18:08:09 +0300

    sprint 3

---
+++ yatube/posts/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render, get_object_or_404
+
+from .models import Post, Group
+
+
+def index(request):
+    template = 'posts/index.html'
+    posts = Post.objects.order_by('-pub_date')[:10]
+
+    context = {
+        'posts': posts
+    }
+
+    return render(request, template, context)
+
+
+# Страница со списком мороженого
+def group_posts(request, slug):
+    template = 'posts/group_list.html'
+    group = get_object_or_404(Group, slug=slug)
+    posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
+
+    context = {
+        'group': group,
+        'posts': posts,
+    }
+
+    return render(request, template, context)
